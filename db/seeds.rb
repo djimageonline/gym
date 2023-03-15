7c77eda0c09b2c276258d3b7859adfb3a30f07f7
Johnny Proano <jproano22@yahoo.com>
2023-03-15 17:51:15 -0400

    added exercise model acnd seed 2 entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+Exercise.create(name: "Push ups", description: "lay on chest and push up", image_url: "https://toughmudder.co.uk/wp-content/uploads/2020/03/jonathan-borba-lrQPTQs7nQQ-unsplash.jpg", video_url: "https://www.youtube.com/watch?v=1fbU_MkV7NE")
+
+Exercise.create(name: "Sit ups", description: "laydown and sit up", image_url: "https://images.unsplash.com/photo-1616803689943-5601631c7fec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", video_url: "https://www.youtube.com/watch?v=IODxDxX7oi4")
